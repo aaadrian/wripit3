b4af7a230e78d3f3487dee9f9befceb3fc6f666a
aaadrian <aaa@drian.de>
2016-09-20 23:22:22 +0200

    Ein Alert - wohoo

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
+
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-  
+  //constructor(public navCtrl: NavController) {
+  //
+  //}
+  constructor(public alertCtrl: AlertController) {
+
   }
+
+doAlert() {
+    
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  
 }
+}
+
+
+
+
